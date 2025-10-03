75d00b2b61365f15a9d4eb82856905bd7166a90a
inno <sayyou0918@gmail.com>
2025-10-04 05:32:37 +0900

    Add Oauth interfaces and config

---
+++ src/main/kotlin/config/OAuth2Config.kt
@@ -0,0 +1,25 @@
+package org.inno.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+/*
+    oauth2:
+        providers:
+            google:
+                client-id : sample
+                client-secret: <>
+                redirect-uri: <>
+            github:
+                client-id : sample
+                client-secret: <>
+                redirect-uri: <>
+*/
+
+@Configuration
+@ConfigurationProperties(prefix = "oauth2")
+class OAuth2Config {
+    val providers: MutableMap<String, OAuth2ProviderValues> = mutableMapOf()
+}
+
+data class OAuth2ProviderValues(val clientId: String, val clientSecret: String, val redirectUri: String)
